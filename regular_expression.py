e64ef0caa6c5459343aa39f9055d055ae3247397
Maskawath <summergub162@gmail.com>
2020-05-14 17:02:53 +0600

    Add files via upload

---
+++ regular_expression.py
@@ -0,0 +1,12 @@
+import re
+
+pattern = re.compile(r"\d\d\d.\d\d\d.\d\d\d\d")
+
+try:
+	with open ("dat.txt" , "r", encoding="utf-8") as f:
+		contents = f.read()
+		matches= pattern.finditer(contents)
+		for match in matches:
+			print(match)
+except IOError:
+    print ("Could not read file:")
